5576aa196231b0a47dd25f20b3c573eff3ebbb5f
mccannbr <mccannbr@tcd.ie>
2024-11-14 15:08:48 +0000

    adding query bias txt

--- gpt.py
+++ gpt.py
@@ -68,8 +68,8 @@ class Head(nn.Module):
 
     def __init__(self, head_size):
         super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=True)
+        self.key = nn.Linear(n_embd, head_size, bias=True)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
